49c754b9938bd74c69fb37a70445b8ca78472955
Jordan Woodruff <boogermanus@gmail.com>
2025-01-03 11:29:32 -0600

    fix bootstrap and setup basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "./components/card/card.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CardComponent]
+  imports: [
+    CardComponent,
+    RouterOutlet
+  ]
 })
 export class AppComponent {
   title = 'Memory';
