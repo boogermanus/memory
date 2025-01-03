ed535f38e5577aa978e61820b0f946f1e43614a9
Jordan Woodruff <boogermanus@gmail.com>
2025-01-03 12:13:29 -0600

    game settings class

---
+++ src/app/models/game-settings.spec.ts
@@ -0,0 +1,7 @@
+import { GameSettings } from './game-settings';
+
+describe('GameSettings', () => {
+  it('should create an instance', () => {
+    expect(new GameSettings()).toBeTruthy();
+  });
+});
---
+++ src/app/models/game-settings.ts
@@ -0,0 +1,6 @@
+export class GameSettings {
+    private boardSize: number = 0;
+    public get BoardSize(): number {
+        return this.boardSize;
+    }
+}
