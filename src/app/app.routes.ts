0919ff910e583a93b07c06f08d9cd421d1a28ace
Jordan Woodruff <boogermanus@gmail.com>
2025-01-03 15:16:12 -0600

    new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { GameSettingsComponent } from './components/game-settings/game-settings.component';
+import { GameBoardComponent } from './components/game-board/game-board.component';
 
 export const routes: Routes = [
-    {path: 'settings', component: GameSettingsComponent }
+    { path: 'settings', component: GameSettingsComponent },
+    { path: 'game', component: GameBoardComponent }
 ];
