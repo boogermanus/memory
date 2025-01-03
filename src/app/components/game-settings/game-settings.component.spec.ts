e50b4c56607d73feaff86caded8173c8b3da96de
Jordan Woodruff <boogermanus@gmail.com>
2025-01-03 15:13:27 -0600

    more testing

--- src/app/components/game-settings/game-settings.component.spec.ts
+++ src/app/components/game-settings/game-settings.component.spec.ts
@@ -2,10 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameSettingsComponent } from './game-settings.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GameService } from '../../services/game.service';
+import { GameSettings } from '../../models/game-settings';
 
 describe('GameSettingsComponent', () => {
   let component: GameSettingsComponent;
   let fixture: ComponentFixture<GameSettingsComponent>;
+  let gameSerivce: GameService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +19,8 @@ describe('GameSettingsComponent', () => {
     })
     .compileComponents();
 
+    gameSerivce = TestBed.inject(GameService);
+
     fixture = TestBed.createComponent(GameSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,4 +29,18 @@ describe('GameSettingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call init when button is clicked', () => {
+    spyOn(gameSerivce, 'initGame');
+    component.startGame();
+    expect(gameSerivce.initGame).toHaveBeenCalled();
+  });
+
+  it('should shuffle when the button is clicked', () => {
+    spyOn(gameSerivce, 'shuffle');
+    component.boardSize = 4;
+    component.startGame();
+    expect(gameSerivce.shuffle).toHaveBeenCalled();
+  })
+
 });
--- src/app/components/game-settings/game-settings.component.ts
+++ src/app/components/game-settings/game-settings.component.ts
@@ -3,6 +3,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { GameService } from '../../services/game.service';
+import { GameSettings } from '../../models/game-settings';
 @Component({
   selector: 'app-game-settings',
   standalone: true,
@@ -18,7 +20,12 @@ import { MatButtonModule } from '@angular/material/button';
 export class GameSettingsComponent {
   public boardSize: number = 0;
 
+  constructor(private readonly gameService: GameService) {
+
+  }
+
   public startGame(): void {
-    
+    this.gameService.initGame(new GameSettings(this.boardSize));
+    this.gameService.shuffle();
   }
 }
