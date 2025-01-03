7a5c0d0ba538e86025afb203990286fcfd61da26
Jordan Woodruff <boogermanus@gmail.com>
2025-01-03 15:48:57 -0600

    testing card layout

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,5 +1,7 @@
 export class Card {
 
+    private readonly DEFAULT_FRONT: string = 'Front';
+    private readonly DEFAULT_BACK: string = 'Back'
     public Id: number = 0 ;
 
     private isFlipped: boolean = false;
@@ -13,8 +15,8 @@ export class Card {
     }
     constructor(id?: number, face?: string, back?:string) {
         this.Id = id ?? 0;
-        this.faceValue = face ?? '';
-        this.backValue = back ?? '';
+        this.faceValue = face ?? this.DEFAULT_FRONT;
+        this.backValue = back ?? this.DEFAULT_BACK;
     }
 
     public state: string = 'inactive'
